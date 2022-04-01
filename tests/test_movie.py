4644c3aeab9ebf548aac982b6b66dd5504e99bed
Megan Korling <korlingm@gmail.com>
2022-04-01 11:39:38 -0700

    created classes and added tests

--- tests/test_movie.py
+++ tests/test_movie.py
@@ -1,9 +1,22 @@
 import pytest
+from viewing_party.movie import Movie
 
-def test_1():
-    # Arrange
-
-    # Act
+def test_movie_attributes():
+    # Arrange/Act
+    movie = Movie("Encanto", "children", 5, "Disney+")
 
     # Assert
-    pass
+    assert movie.title == "Encanto"
+    assert movie.genre == "children"
+    assert movie.rating == 5
+    assert movie.host == "Disney+"
+
+def test_movie_rating_updates():
+    # Act
+    movie = Movie("Encanto", "children", 5, "Disney+")
+
+    # Arrange
+    movie.update_rating(10)
+
+    # Assert
+    assert movie.rating == 10
--- tests/test_person.py
+++ tests/test_person.py
@@ -1,9 +1,39 @@
 import pytest
+from viewing_party.person import Person
+from viewing_party.movie import Movie
 
-def test_1():
-    # Arrange
 
-    # Act
+def test_person_attributes_set_up():
+    # Arrange/Act
+    movie = Movie("The Avengers", "action", 4, "Disney+")
+    person = Person("Diana", [], [movie], ["Disney+"])
 
     # Assert
-    pass
+    assert person.name == "Diana"
+    assert person.watched == []
+    assert person.friends == [movie]
+    assert person.subscriptions == ["Disney+"]
+
+def test_adds_movie():
+    # Arrange
+    movie = Movie("The Avengers", "action", 4, "Disney+")
+    person = Person("Diana", [], [], ["Disney+"])
+
+    # Act
+    person.add_watched(movie)
+
+    # Assert
+    assert movie in person.watched
+    assert len(person.watched) == 1
+
+def test_get_num_watched():
+    # Arrange
+    movie1 = Movie("The Avengers", "action", 4, "Disney+")
+    movie2 = Movie("Talladega Nights", "comedy", 4, "Netflix")
+    person = Person("Megan", [movie1, movie2], [], ["Netflix"])
+
+    # Act
+    result = person.get_num_watched()
+
+    # Assert
+    assert result == 2
